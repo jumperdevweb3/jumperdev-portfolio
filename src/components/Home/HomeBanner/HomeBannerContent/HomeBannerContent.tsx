0d68a1b0d64774a0b64a35c2ff363790574ebe74
jumperdevweb3 <82238474+skoczy01@users.noreply.github.com>
2023-01-15 22:27:10 +0100

    Feat: Add spring animation

--- src/components/Home/HomeBanner/HomeBannerContent/HomeBannerContent.tsx
+++ src/components/Home/HomeBanner/HomeBannerContent/HomeBannerContent.tsx
@@ -1,31 +1,55 @@
-import { useEffect, useRef } from "react";
-
+import { ReactNode, useEffect, useState } from "react";
+import { useTrail, a } from "@react-spring/web";
+import React from "react";
 export const HomeBannerContent = () => {
-  const textBoxRef = useRef<HTMLDivElement>(null);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
-    textBoxRef.current?.classList.add("animate-fromLeft");
-    textBoxRef.current?.classList.remove("-translate-x-full");
+    setOpen(true);
   }, []);
   return (
     <div
-      ref={textBoxRef}
-      className="flex -translate-x-full flex-col gap-2 text-center font-inter text-4xl font-bold md:text-left"
+      className={
+        "flex h-auto max-h-[250px] min-h-[200px] flex-col gap-2 text-center font-inter text-4xl font-bold md:text-left"
+      }
     >
-      <p>Hi, 🫣</p>
-      <p>My name is </p>
-      <p>
-        <span className="bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-[transparent]">
-          Filip
-        </span>
-      </p>
-      <p>I build things</p>
-      <p>
-        ...and i love{" "}
-        <span className="bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-[transparent]">
-          web3
-        </span>
-      </p>
+      <Trail open={open}>
+        <p>Hi, 🫣</p>
+        <p>My name is </p>
+        <p>
+          <span className="bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-[transparent]">
+            Filip
+          </span>
+        </p>
+        <p>I build things</p>
+        <p>
+          ...and i love{" "}
+          <span className="bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-[transparent]">
+            web3
+          </span>
+        </p>
+      </Trail>
+    </div>
+  );
+};
+
+const Trail = ({ open, children }: { children: ReactNode; open: boolean }) => {
+  const items = React.Children.toArray(children);
+  const trail = useTrail(items.length, {
+    config: { mass: 5, tension: 2000, friction: 200 },
+    opacity: open ? 1 : 0,
+    x: open ? 0 : 80,
+    height: "auto",
+    from: { opacity: 0, x: 20, height: 0 },
+    delay: 900,
+  });
+  return (
+    <div>
+      {trail.map(({ height, ...style }, index) => (
+        <a.div key={index} style={style}>
+          <a.div style={{ height }}>{items[index]}</a.div>
+        </a.div>
+      ))}
     </div>
   );
 };
