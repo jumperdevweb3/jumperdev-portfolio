84d328a952e0258d77cb94179b47f2bcb867d7c1
Filip <82238474+skoczy01@users.noreply.github.com>
2023-01-16 14:51:27 +0100

    Feat: Fix mobile height function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import "/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "@/ui/Layout/Layout";
+import { useEffect } from "react";
+import { useWindowSize } from "src/hooks/use-windowSize";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { width } = useWindowSize();
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      (document.querySelector(":root") as HTMLElement).style.setProperty(
+        "--vh",
+        window.innerHeight / 100 + "px"
+      );
+    }
+  }, [width]);
   return (
     <Layout>
       <Component {...pageProps} />
