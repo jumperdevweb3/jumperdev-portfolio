45bb42419214e59f6a0a4cd6df74e1459a51e96b
Filip <82238474+skoczy01@users.noreply.github.com>
2023-01-18 14:36:15 +0100

    Refactor: file name change

---
+++ src/components/Home/ScrollSection/ScrollSection.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState, useRef } from "react";
+import { SideScroll } from "../SideScroll/SideScroll";
+
+export const ScrollSection = () => {
+  const cardsContent = [
+    "Typescript",
+    "React",
+    "Next.js",
+    "SCSS",
+    "TailwindCSS",
+  ];
+  const [offset, setOffset] = useState(0);
+  const promoSquareRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const countOfSquaresThatWillFitIntoViewport = window.innerWidth / 700;
+    const fullSquaresThatWillFit = Math.floor(
+      countOfSquaresThatWillFitIntoViewport
+    );
+    const offsetOfSquares =
+      ((countOfSquaresThatWillFitIntoViewport - fullSquaresThatWillFit) *
+        (promoSquareRef?.current?.offsetWidth || 700)) /
+      2;
+    setOffset(offsetOfSquares);
+  }, []);
+
+  return (
+    <>
+      <SideScroll offset={offset}>
+        {cardsContent.map((text, index) => (
+          <div
+            ref={promoSquareRef}
+            key={index}
+            className="before: grid h-[40vh] w-[700px] max-w-[80vw] skew-y-[0deg] animate-cardSkew place-items-center rounded-xl border-[3px] border-solid border-[#bbb] bg-test-dark-purple bg-cover bg-no-repeat shadow-m-box-shadow shadow-m-shadow-card"
+          >
+            <p className="text-center text-2xl font-bold md:text-5xl ">
+              {text}
+            </p>
+          </div>
+        ))}
+      </SideScroll>
+    </>
+  );
+};
