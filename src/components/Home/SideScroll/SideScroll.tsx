285dfc89a103e89284966d7c50bd11a2b7805cf6
Filip <82238474+skoczy01@users.noreply.github.com>
2023-01-18 14:37:01 +0100

    Refactor: change styles & imports

--- src/components/Home/SideScroll/SideScroll.tsx
+++ src/components/Home/SideScroll/SideScroll.tsx
@@ -1,80 +1,84 @@
-import { ReactNode } from '@mdx-js/react/lib'
-import {
-  motion,
-  useMotionValue,
-  useScroll,
-  useSpring,
-  useTransform,
-} from 'framer-motion'
-import { useCallback, useEffect, useRef, useState } from 'react'
-import classes from './SideScroll.module.scss'
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
 
 type Props = {
-  children: ReactNode
-  offset?: number
-  scrollDistanceMultiplier?: number
-}
+  children: ReactNode;
+  offset?: number;
+  scrollDistanceMultiplier?: number;
+};
 
 export const SideScroll = ({
   children,
   offset = 0,
   scrollDistanceMultiplier = 1,
 }: Props) => {
-  const scrollListenerRef = useRef<HTMLDivElement>(null)
-  const ghostRef = useRef(null)
-  const scrollingContentRef = useRef<HTMLDivElement>(null)
-  const stickyContainerRef = useRef<HTMLDivElement>(null)
-  const [scrollRange, setScrollRange] = useState(0)
-  const [viewportW, setViewportW] = useState(0)
+  const scrollListenerRef = useRef<HTMLDivElement>(null);
+  const ghostRef = useRef(null);
+  const scrollingContentRef = useRef<HTMLDivElement>(null);
+  const stickyContainerRef = useRef<HTMLDivElement>(null);
+  const [scrollRange, setScrollRange] = useState(0);
+  const [viewportW, setViewportW] = useState(0);
 
   const { scrollYProgress } = useScroll({
     target: scrollListenerRef,
-  })
+  });
 
   const onResizeScrollRange = useCallback((entries: ResizeObserverEntry[]) => {
     for (let entry of entries) {
       setScrollRange(
         entry.contentRect.width +
           (window.innerWidth - document.documentElement.clientWidth)
-      )
+      );
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
     const resizeObserver = new ResizeObserver((entries) =>
       onResizeScrollRange(entries)
-    )
+    );
     scrollingContentRef &&
       scrollingContentRef.current &&
-      resizeObserver.observe(scrollingContentRef.current)
-  }, [onResizeScrollRange])
+      resizeObserver.observe(scrollingContentRef.current);
+  }, [onResizeScrollRange]);
 
   const onResize = useCallback((entries: ResizeObserverEntry[]) => {
     for (let entry of entries) {
-      setViewportW(entry.contentRect.width)
+      setViewportW(entry.contentRect.width);
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
-    const resizeObserver = new ResizeObserver((entries) => onResize(entries))
-    ghostRef && ghostRef.current && resizeObserver.observe(ghostRef.current)
-    return () => resizeObserver.disconnect()
-  }, [onResize])
+    const resizeObserver = new ResizeObserver((entries) => onResize(entries));
+    ghostRef && ghostRef.current && resizeObserver.observe(ghostRef.current);
+    return () => resizeObserver.disconnect();
+  }, [onResize]);
 
   const transform = useTransform(
     scrollYProgress,
     [0, 1],
     [0 + offset, -scrollRange + viewportW - offset]
-  )
-  const physics = { damping: 15, mass: 0.27, stiffness: 100 }
-  const spring = useSpring(transform, physics)
+  );
+  const physics = { damping: 15, mass: 0.27, stiffness: 100 };
+  const spring = useSpring(transform, physics);
 
   return (
-    <div className={classes.root} ref={scrollListenerRef}>
-      <div ref={stickyContainerRef} className={classes.scroll}>
+    <div
+      className={
+        "relative after:pointer-events-none after:absolute after:top-0 after:left-0 after:h-full after:w-full after:content-['']"
+      }
+      ref={scrollListenerRef}
+    >
+      <div
+        ref={stickyContainerRef}
+        className={
+          "sticky top-0 left-0 right-0 overflow-x-hidden will-change-transform"
+        }
+      >
         <motion.section
           style={{ x: spring }}
-          className={classes.thumbnailsContainer}
+          className={
+            "relative top-0 flex h-[calc(var(--vh)_*_100)] w-max items-center gap-4"
+          }
           ref={scrollingContentRef}
         >
           {children}
@@ -83,8 +87,8 @@ export const SideScroll = ({
       <div
         ref={ghostRef}
         style={{ height: scrollRange * scrollDistanceMultiplier }}
-        className={classes.ghost}
+        className={"w-screen"}
       />
     </div>
-  )
-}
+  );
+};
