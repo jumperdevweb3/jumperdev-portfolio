a600075687e6fc3ab0737377c5c02d6cbd07ca7a
jumperdevweb3 <82238474+skoczy01@users.noreply.github.com>
2023-01-19 20:53:41 +0100

    Feat: add studied list

---
+++ src/components/Home/KnowledgeSources/studiedLists.ts
@@ -0,0 +1,61 @@
+export const coursesList = [
+  {
+    id: 1,
+    name: "React",
+    description: "The Complete Guide (incl Hooks, React Router, Redux)",
+    author: "Maximilian Schwarzmüller",
+    image: "/reactjs-course.jpg",
+    url: "/",
+  },
+  {
+    id: 2,
+    name: "Next.js",
+    description: "The Complete Guide (incl. Two Paths!)",
+    author: "Maximilian Schwarzmüller",
+    image: "/nextjs-course.jpg",
+    url: "/",
+  },
+  {
+    id: 3,
+    name: "Understanding TypeScript ",
+    description: "2022 Edition",
+    author: "Maximilian Schwarzmüller",
+    image: "/typescript-course.jpg",
+    url: "/",
+  },
+];
+
+export const youtubeChannels = [
+  {
+    id: 0,
+    name: "Jack Herrington",
+    description:
+      "Frontend videos from basic to very advanced; tutorials, technology deep dives. ",
+    url: "https://www.youtube.com/@jherr",
+    image: "/jack-herrington.png",
+  },
+  {
+    id: 1,
+    name: "Fireship",
+    description:
+      "High-intensity ⚡ code tutorials to help you build & ship your app faster.",
+    url: "https://www.youtube.com/@Fireship",
+    image: "/fireship.png",
+  },
+  {
+    id: 2,
+    name: "CoderOne",
+    description:
+      "CoderOne is a Learning Channel For Web Development, Software Enginnering and Coding All in one place with a High quality Video/Written Tutorials.",
+    url: "https://www.youtube.com/@CoderOne",
+    image: "/coder-one.png",
+  },
+  {
+    id: 3,
+    name: "Web Dev Simplified",
+    description:
+      "Web Dev Simplified is all about teaching web development skills and techniques in an efficient and practical manner.",
+    url: "https://www.youtube.com/@WebDevSimplified",
+    image: "/web-dev-simplified.png",
+  },
+];
