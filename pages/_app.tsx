e6389ac5f15e76171b72fd9187a402f1470de84c
jumperdevweb3 <82238474+skoczy01@users.noreply.github.com>
2023-01-22 19:27:59 +0100

    Fix: use effect dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import { useWindowSize } from "src/hooks/use-windowSize";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { width } = useWindowSize();
+  const { width, height } = useWindowSize();
   useEffect(() => {
     if (typeof window !== "undefined") {
       (document.querySelector(":root") as HTMLElement).style.setProperty(
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
         window.innerHeight / 100 + "px"
       );
     }
-  }, [width]);
+  }, [width, height]);
   return (
     <Layout>
       <Component {...pageProps} />
