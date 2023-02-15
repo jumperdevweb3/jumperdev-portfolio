9b61b1189c45583bf047035352babd69693fa47d
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2023-02-15 22:30:49 +0100

    refactor

--- src/components/Home/Projects/Projects.tsx
+++ src/components/Home/Projects/Projects.tsx
@@ -1,35 +1,34 @@
+import { SectionTitle } from "@/ui/SectionTitle/SectionTitle";
 import Image from "next/image";
 
 export const Projects = () => {
   return (
-    <section className="min-h-[80vh]">
-      <div className="relative flex w-full justify-center">
-        <div className="relatve h-auto w-[50%]">
-          <div className=" h-[5rem] w-[40%]">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-              vitae id amet voluptas, expedita natus, rerum optio iure laborum
-              doloremque iste sapiente consectetur dicta nihil, quasi vero?
-              Dolores, necessitatibus aliquam.
-            </p>
-          </div>
-        </div>
-        <div className="relative h-full w-[40%]">
-          <Image
-            src={"/crypto_project1.png"}
-            width={500}
-            height={500}
-            alt="crypto project"
-            className="w-100% h-auto"
-          />
-          <Image
-            src={"/crypto_project1_2.png"}
-            width={500}
-            height={500}
-            alt="crypto project"
-            className="w-100% -translate-y-[50% absolute top-[20%] left-[70%] -z-10 h-auto -translate-x-[50%]"
-          />
-        </div>
+    <section className="relative flex min-h-[100vh] flex-col gap-8 px-4">
+      <SectionTitle title="My bigest" subTitle="Project" />
+      <div className="rounded-xl bg-slate-500 bg-[#5a0f8d59] p-8 backdrop-blur-sm">
+        <p className="font-fira text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto vitae
+          id amet voluptas, expedita natus, rerum optio iure laborum doloremque
+          iste sapiente consectetur dicta nihil, quasi vero? Dolores,
+          necessitatibus aliquam.
+        </p>
+      </div>
+
+      <div className="relative w-full">
+        <Image
+          src={"/test2.png"}
+          width={500}
+          height={500}
+          alt="project image"
+          className="max-w-[85%] rounded-md border-[2px] border-white"
+        />
+        <Image
+          src={"/test1.png"}
+          width={500}
+          height={500}
+          alt="project image"
+          className="absolute top-[30%] left-[15%] -z-10 max-w-[85%] rounded-md border-[2px] border-white"
+        />
       </div>
     </section>
   );
