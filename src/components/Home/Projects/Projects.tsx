3a29675ff3bd5b5cab17d503099e464922ae0000
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2023-02-16 21:47:23 +0100

    Refactor

--- src/components/Home/Projects/Projects.tsx
+++ src/components/Home/Projects/Projects.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./Projects.module.scss";
 export const Projects = () => {
   return (
@@ -8,16 +9,24 @@ export const Projects = () => {
         <div className={styles["text-container"]}>
           <div className={styles["text-wrapper"]}>
             <h2 className={styles.title}>Crypto Tracker App</h2>
-            <h3 className={styles["sub-title"]}>Tech Stack</h3>
-            <p className={styles.stack}>
-              React - Next.js - Redux - css modules
-            </p>
+            <div className="m-4 flex flex-col gap-4">
+              <h3 className={styles["sub-title"]}>Tech Stack 🛠️</h3>
+              <div className={styles.stack}>
+                <p>React * React * React * React</p>
+              </div>
+            </div>
             <p className={styles.description}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
               vitae id amet voluptas, expedita natus, rerum optio iure laborum
               doloremque iste sapiente consectetur dicta nihil, quasi vero?
               Dolores, necessitatibus aliquam.
             </p>
+            <Link
+              href={""}
+              className="w-[180px] rounded-xl bg-[#6208A8] py-1 duration-150 hover:bg-[#7733AD]"
+            >
+              <span className="mx-2">🔍</span>Explore
+            </Link>
           </div>
         </div>
         <div className={styles["images-wrapper"]}>
