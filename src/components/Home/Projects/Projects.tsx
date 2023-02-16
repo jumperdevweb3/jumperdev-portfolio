a89ab05942894d2283a3c6a2c7c1766606c74a2c
Filip <82238474+skoczy01@users.noreply.github.com>
2023-02-16 14:57:42 +0100

    Refactor: change view of project

--- src/components/Home/Projects/Projects.tsx
+++ src/components/Home/Projects/Projects.tsx
@@ -1,34 +1,41 @@
-import { SectionTitle } from "@/ui/SectionTitle/SectionTitle";
 import Image from "next/image";
-
+import styles from "./Projects.module.scss";
 export const Projects = () => {
   return (
-    <section className="relative flex min-h-[100vh] flex-col gap-8 px-4">
-      <SectionTitle title="My bigest" subTitle="Project" />
-      <div className="rounded-xl bg-slate-500 bg-[#5a0f8d59] p-8 backdrop-blur-sm">
-        <p className="font-fira text-lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto vitae
-          id amet voluptas, expedita natus, rerum optio iure laborum doloremque
-          iste sapiente consectetur dicta nihil, quasi vero? Dolores,
-          necessitatibus aliquam.
-        </p>
-      </div>
-
-      <div className="relative w-full">
-        <Image
-          src={"/test2.png"}
-          width={500}
-          height={500}
-          alt="project image"
-          className="max-w-[85%] rounded-md border-[2px] border-white"
-        />
-        <Image
-          src={"/test1.png"}
-          width={500}
-          height={500}
-          alt="project image"
-          className="absolute top-[30%] left-[15%] -z-10 max-w-[85%] rounded-md border-[2px] border-white"
-        />
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.overlay}></div>
+        <div className={styles["text-container"]}>
+          <div className={styles["text-wrapper"]}>
+            <h2 className={styles.title}>Crypto Tracker App</h2>
+            <h3 className={styles["sub-title"]}>Tech Stack</h3>
+            <p className={styles.stack}>
+              React - Next.js - Redux - css modules
+            </p>
+            <p className={styles.description}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+              vitae id amet voluptas, expedita natus, rerum optio iure laborum
+              doloremque iste sapiente consectetur dicta nihil, quasi vero?
+              Dolores, necessitatibus aliquam.
+            </p>
+          </div>
+        </div>
+        <div className={styles["images-wrapper"]}>
+          <Image
+            src={"/test2.png"}
+            width={500}
+            height={500}
+            alt="crypto project"
+            className={styles.image}
+          />
+          <Image
+            src={"/test1.png"}
+            width={500}
+            height={500}
+            alt="crypto project"
+            className={styles.image}
+          />
+        </div>
       </div>
     </section>
   );
