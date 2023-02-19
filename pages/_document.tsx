803eb36e319302923c443cb674ba4ae0aa72b549
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2023-02-19 10:58:45 +0100

    Feat: add google tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="gMU-2OIHk2cm2agH6XKONEgfiXvEO-6iS7QjLDs3mA8"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
